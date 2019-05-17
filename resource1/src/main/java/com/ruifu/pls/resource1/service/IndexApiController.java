8c16a4f86c647a7fda0fef696a582b04942cd286
sfu <12345678>
2019-05-17 16:37:32 +0800

    add authorization server

--- resource1/src/main/java/com/ruifu/pls/resource1/service/IndexApiController.java
+++ resource1/src/main/java/com/ruifu/pls/resource1/service/IndexApiController.java
@@ -1,19 +1,32 @@
 package com.ruifu.pls.resource1.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api")
+@RefreshScope
 public class IndexApiController {
 
+    @Value("${scope.refresh}")
+    public String profile;
+
+    @RequestMapping("/profile")
+    public String profile(){
+        return profile;
+    }
 
     @Value("${server.port}")
     private String serverPort;
 
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @RequestMapping("/index")
     public String index(){
-        return "Version 1.0, running on port: "+serverPort;
+        return "Version 1.0, running on port: "+serverPort+"-"+password;
     }
 }
